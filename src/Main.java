1343e874fbccac07fe4ae2a4d96b8ab8e8e4ed30
sikoruben <79188659+sikoruben@users.noreply.github.com>
2023-03-21 13:29:52 +0200

    commit3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,72 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //public,void stb a String utan
+        // https://www.w3schools.com/java/java_ref_keywords.asp
+        String publics = "";
+        //single line comments
+        /*
+            multi
+            line
+            comment
+         */
+        System.out.println("Hello Java");
+        System.out.println();
+        System.out.println(10 +10);
+        /*if(true){
+            
+        }*/
+        //type sout
+        System.out.println();
+        //primitives
+        int number1 = 20;
+        int number2 = 20;
+        int result = number2 + number1;
+        System.out.println(number1+number2);
+        double pi = 3.14;
+        //boolean isAdult = 3;
+        boolean isAdult = false;
+        char a = 'A';
+        //primitive data type 01:01:14 amigoscode
+        //byte n = 128; - error
+        byte n = 127;
+        //short m = 33000; - error
+        short m = 128;
+        int amount = 1_000_000_008; // wow, just wow
+        double amount2 = 1_000_000.31;
+        System.out.println(amount);
+        System.out.println(amount2);
+
+        int numberOne = 10;
+        int numberTwo = 3;
+        System.out.println(numberOne + numberTwo);
+        System.out.println(numberOne - numberTwo);
+        System.out.println(numberOne * numberTwo);
+        System.out.println(numberOne / numberTwo);
+        System.out.println(numberOne % numberTwo);
+        // BODMAS Brackets Orders Division Multiplication Addition Substraction
+        System.out.println((2+2)* (3/1)* 2);
+
+        //increment/decrement operator ++/--
+        int number = 0;
+        System.out.println(number++);
+        System.out.println(number);
+        int numberTwo2 = 0;
+        System.out.println(++numberTwo2);
+        System.out.println(numberTwo2);
+        int numberThree = 0;
+        System.out.println(numberThree--);
+        System.out.println(numberThree);
+        numberThree = 0;
+        System.out.println(--numberThree);
+        System.out.println(numberThree);
+        /*
+        int increment = number + 1;
+        int decrement = number -1;
+        System.out.println(increment);
+        System.out.println(decrement);
+         */
+
     }
     //ez egy komment lol
     //komment2xD
